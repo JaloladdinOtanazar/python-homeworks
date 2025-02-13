6baac7624190133095e5743641981dd57eab2c66
Jalolbek <j.otanazarov@newuu.uz>
2025-02-13 23:36:18 +0500

    lesson-2/homework

---
+++ string6.py
@@ -0,0 +1,6 @@
+a = input("enter the 1st one: ")
+b = input("enter the 2nd one: ")
+if b in a:
+    print(a, f"is found in {b}")
+else:
+    print("nothing happened")
---
+++ string9.py
@@ -0,0 +1,4 @@
+string = input("enter a string: ")
+reversed = string[::-1]
+print("the new string is", reversed)
+
