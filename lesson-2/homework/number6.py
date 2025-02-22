e333874a31fa28e509cd6d7ec7522cdf73156e50
JaloladdinOtanazar <161422647+JaloladdinOtanazar@users.noreply.github.com>
2025-02-22 20:22:55 +0500

    Delete lesson-2 directory

--- lesson-2/homework/number6.py
+++
@@ -1,3 +0,0 @@
-a = float(input("enter 1st number: "))
-last_digit = a % 10
-print("last digit is ", last_digit)
--- lesson-2/homework/string13.py
+++
@@ -1,3 +0,0 @@
-string=input("enter a string:")
-new_string = string.replace(" ", "")
-print("the new string is ", new_string)
