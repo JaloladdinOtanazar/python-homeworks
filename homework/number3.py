b911d8b0ea855147cda618719afc80e1966486fc
JaloladdinOtanazar <161422647+JaloladdinOtanazar@users.noreply.github.com>
2025-02-22 20:27:51 +0500

    lesson-2/homework

---
+++ homework/number3.py
@@ -0,0 +1,4 @@
+a = float(input("Enter a distace in kilometers: "))
+a_in_meters = a*1000
+a_in_sentimeters = a*100000
+print("the distance in meters: ", a_in_meters, "the distance in sentimeters: ", a_in_sentimeters)
---
+++ homework/string8.py
@@ -0,0 +1,2 @@
+string = input("enter a string: ")
+print(f"the first letter is {string[0]}, the last letter is {string[-1]}")
